784f7412381ad5e32e787b403504919b54e917db
sksinghjhs16 <sksingh.jhs@gmail.com>
2021-12-22 13:21:37 +0530

    bot updated

--- setup.py
+++ setup.py
@@ -117,5 +117,5 @@ def getMessage():
     return "!", 200
 
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
