36f9c3cca566f9aeabdea3d69875226fd25f0f3c
sksinghjhs16 <sksingh.jhs@gmail.com>
2021-12-31 17:44:42 +0530

    get_owe

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 from helpers import *
 import os
 
+
 def is_command(txt):
     return txt[0] == "/"
 
@@ -15,7 +16,7 @@ def execute_command(command, chat_id):
                 for index, item in enumerate(file):
                     result += f"{index+1}. {item['item']}\n"
                 broadcast_msg(chat_id, result)
-        else :
+        else:
             broadcast_msg(chat_id, "Nil pending item.")
     elif command[:13] == "/pending add ":
         item = command[13:]
@@ -26,15 +27,16 @@ def execute_command(command, chat_id):
                 with open("pendingItems.pkl", "wb") as keyPickle:
                     pickle.dump(newList, keyPickle)
                     broadcast_msg(chat_id, "New item added successfully.")
-        else :
+        else:
             newList = [{"item": item, "chat_id": chat_id}]
             with open("pendingItems.pkl", "wb") as keyPickle:
-                    pickle.dump(newList, keyPickle)
-                    broadcast_msg(chat_id, "New item added successfully.")
+                pickle.dump(newList, keyPickle)
+                broadcast_msg(chat_id, "New item added successfully.")
     elif command[:13] == "/pending del ":
         itemNum = command[13:]
-        try: itemNum = int(itemNum)
-        except: 
+        try:
+            itemNum = int(itemNum)
+        except:
             broadcast_msg(chat_id, "Kindly enter integer number only.")
             return
         if os.path.isfile("pendingItems.pkl"):
@@ -44,10 +46,13 @@ def execute_command(command, chat_id):
                 if itemNum < 1 or itemNum > len(file):
                     broadcast_msg(chat_id, "Kindly enter valid pending item number.")
                     return
-                if file[itemNum-1]['chat_id'] == chat_id:
-                    del file[itemNum-1:itemNum]
+                if file[itemNum - 1]["chat_id"] == chat_id:
+                    del file[itemNum - 1 : itemNum]
                 else:
-                    broadcast_msg(chat_id, f"Item no. {itemNum} has not been created by you. Kindly enter valid item no. only.")
+                    broadcast_msg(
+                        chat_id,
+                        f"Item no. {itemNum} has not been created by you. Kindly enter valid item no. only.",
+                    )
                     return
                 newList = [*file]
                 with open("pendingItems.pkl", "wb") as keyPickle:
@@ -56,27 +61,33 @@ def execute_command(command, chat_id):
             if len(newList) == 0:
                 os.remove("pendingItems.pkl")
                 return
-        else :
+        else:
             broadcast_msg(chat_id, "No pending item exists for deletion.")
     elif command == "/help":
-        help_msg = "<pre>Following are the options:</pre>"
+        help_msg = "Following are the options:\n"
+        item1 = "1. /pending add item_description - to add an item to pending list\n2. /pending del item_number - to delete an item from pending list\n"
+        help_msg += item1
         broadcast_msg(chat_id, help_msg)
-    elif (
-        command[:5] == "/all "
-        and chat_id == 44114772
-        and command[5:8] == "img"
-    ):
+    elif command[:5] == "/all " and chat_id == 44114772 and command[5:8] == "img":
         data = {"type": "image"}
         with open("img.pkl", "wb") as imgFile:
             pickle.dump(data, imgFile)
-    elif (
-        command[:5] == "/all "
-        and chat_id == 44114772
-        and command[5:9] == "file"
-    ):
+    elif command[:5] == "/all " and chat_id == 44114772 and command[5:9] == "file":
         data = {"type": "file"}
         with open("file.pkl", "wb") as imgFile:
             pickle.dump(data, imgFile)
+    elif command == "/get_owe_pu":
+        if os.path.isfile("owe_pu.pkl"):
+            with open("owe_pu.pkl", "rb") as keyPickle:
+                file = pickle.load(keyPickle)
+                broadcast_items(chat_id, file["owe_pu_file_id"], "Document")
+            return
+        resp = sendFile(chat_id, "excel", "files\OWE_Nov_21.xlsx", "OWE_Nov_21.xlsx")
+        file_id = json.loads(resp)["result"]["document"]["file_id"]
+        data = {"owe_pu_file_id": file_id}
+        with open("owe_pu.pkl", "wb") as file:
+            pickle.dump(data, file)
+
     elif command[:5] == "/all " and chat_id == 44114772:
         msg = command[5:]
         broadcastToAll(msg)
--- helpers.py
+++ helpers.py
@@ -5,33 +5,43 @@ from database import get_all_users
 API_KEY = config("API_KEY")
 API_URL = f"https://api.telegram.org/bot{API_KEY}"
 
+
 def sendFile(chat_id, type, file_path, file_name):
     url = f"{API_URL}/send"
     payload = {"chat_id": chat_id}
+    url = f"{url}Document"
+    doc_type = ""
     if type == "txt":
-        url = f"{url}Document"
-        files = [
+        doc_type = "text/plain"
+    elif type == "excel":
+        doc_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    elif type == "pdf":
+        doc_type = "application/pdf"
+    files = [
+        (
+            "document",
             (
-                "document",
-                (
-                    f"{file_name}",
-                    open(f"{file_path}", "rb"),
-                    "text/plain",
-                ),
-            )
-        ]
+                f"{file_name}",
+                open(f"{file_path}", "rb"),
+                f"{doc_type}",
+            ),
+        )
+    ]
     resp = requests.request("POST", url, data=payload, files=files)
     return json.dumps(resp.json())
+
 
 def broadcast_all(func, chat_ids, *args):
     for chat_id in chat_ids:
         func(chat_id, *args)
+
 
 def broadcast_msg(chat_id, msg):
     to_url = f"{API_URL}/sendMessage"
     payload = {"chat_id": chat_id, "text": msg, "parse_mode": "HTML"}
     resp = requests.post(to_url, json=payload)
     return json.dumps(resp.json())
+
 
 def broadcast_items(chat_id, item, type):
     to_url = f"{API_URL}/send{type}"
@@ -43,6 +53,7 @@ def broadcast_items(chat_id, item, type):
         payload = {"chat_id": chat_id, "document": item}
     resp = requests.post(to_url, json=payload)
     return json.dumps(resp.json())
+
 
 def parse_request(req):
     chat_id = req["message"]["chat"]["id"]
@@ -59,10 +70,12 @@ def parse_request(req):
     username = req["message"]["chat"]["username"]
     return chat_id, txt, first_name, username
 
+
 def broadcastToAll(msg):
     users = get_all_users()
     for user in users:
         broadcast_msg(user["chatId"], msg)
+
 
 def broadcast_admin(msg):
     broadcast_msg("44114772", msg)
