923849217368130803fd9ae7427e015a1ead3ebb
sksinghjhs16 <sksingh.jhs@gmail.com>
2022-01-27 17:14:28 +0530

    more owe commands added

--- dataCommands.py
+++ dataCommands.py
@@ -12,6 +12,39 @@ def execute_data_command(command, chat_id):
             broadcast_msg(chat_id, "Invalid input provided.")
             return
         data1 = data["data1"]
+        if len(cmd) == 2:
+            pu = cmd[1]
+            puData1 = data1[pu]["Staff Cost"]
+            puData2 = data1[pu]["Non-Staff Cost"]
+            puData3 = data1[pu]["Net"]
+            msg = ""
+            for index, value in enumerate(puData1):
+                if index == 0:
+                    msg += f"Staff Cost >\nD{index+3}: {value} thousand\n"
+                elif index == 11:
+                    msg += f"Total: {value} thousand\n"
+                    broadcast_msg(chat_id, msg)
+                    msg = ""
+                else:
+                    msg += f"D{index+3}: {value} thousand\n"
+            for index, value in enumerate(puData2):
+                if index == 0:
+                    msg += f"Non-Staff Cost >\nD{index+3}: {value} thousand\n"
+                elif index == 11:
+                    msg += f"Total: {value} thousand\n"
+                    broadcast_msg(chat_id, msg)
+                    msg = ""
+                else:
+                    msg += f"D{index+3}: {value} thousand\n"
+            for index, value in enumerate(puData3):
+                if index == 0:
+                    msg += f"Net Total >\nD{index+3}: {value} thousand\n"
+                elif index == 11:
+                    msg += f"Total: {value} thousand\n"
+                    broadcast_msg(chat_id, msg)
+                    msg = ""
+                else:
+                    msg += f"D{index+3}: {value} thousand\n"
         if len(cmd) >= 3:
             pu = cmd[2]
             if pu not in data1.keys():
