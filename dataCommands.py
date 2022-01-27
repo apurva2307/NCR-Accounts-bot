52bea9ee48619cd9f3f795a8bd40e20d265c77e3
sksinghjhs16 <sksingh.jhs@gmail.com>
2022-01-27 14:08:33 +0530

    owe added

--- dataCommands.py
+++ dataCommands.py
@@ -5,10 +5,10 @@ from helpers import *
 def execute_data_command(command, chat_id):
     command = command.lower()
 
-    if command[0:4] == "owe " and len(command) >= 13 and len(command) > 14:
+    if command[0:4] == "owe " and len(command) >= 13:
         data = get_owe_data(command[4:9])
         data1 = data["data1"]
         puData = data1[command[10:]]
         broadcast_msg(chat_id, f"{puData}")
     else:
-        broadcast_msg(chat_id, "No such command exists..")
+        broadcast_msg(chat_id, "No such data command exists..")
