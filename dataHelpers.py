b8d0c16b50d34f2aa702eda757733cc953069ca7
sksinghjhs16 <sksingh.jhs@gmail.com>
2022-01-28 17:58:20 +0530

    minor changes

--- dataHelpers.py
+++ dataHelpers.py
@@ -4,12 +4,10 @@ from helpers import *
 
 def get_data_type_two(title, puData1, puData2, percent):
     showPercent = "%" if percent else ""
-    msg = ""
+    msg = f"{title}\n <b>Fig. in thousand</b>"
     for index, value in enumerate(puData1):
-        if index == 0:
-            msg += f"{title}\nD{index+3}: {value} thousand ({puData2[index]}{showPercent})\n"
-        elif index == 11:
-            msg += f"Total: {value} thousand ({puData2[index]}{showPercent})\n"
+        if index == 11:
+            msg += f"Total: {value} ({puData2[index]}{showPercent})\n"
         else:
-            msg += f"D{index+3}: {value} thousand ({puData2[index]}{showPercent})\n"
+            msg += f"D{index+3}: {value} ({puData2[index]}{showPercent})\n"
     return msg
