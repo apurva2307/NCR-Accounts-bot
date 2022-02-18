5e5ff00dbbf9e438ae2dd64cbfb0efadd6aeb12c
sksinghjhs16 <sksingh.jhs@gmail.com>
2022-02-18 17:37:01 +0530

    owe summary added3

--- dataCommands.py
+++ dataCommands.py
@@ -22,6 +22,7 @@ def execute_data_command(command, chat_id):
             broadcast_msg(chat_id, data["msg"])
             return
         data1 = data["data1"]
+        data2 = data["data2"]
         if len(cmd) == 2:
             puData1 = data1["STAFF"]["toEndActuals"]
             puData1Util = data1["STAFF"]["budgetUtilization"]
@@ -88,7 +89,6 @@ def execute_data_command(command, chat_id):
                     )
                     broadcast_msg(chat_id, message)
                 if pu in rowsMap:
-                    data2 = data["data2"]
                     print(data2)
                     msg = showSummary(pu, data2)
                     broadcast_msg(chat_id, "Data2 extracted.")
